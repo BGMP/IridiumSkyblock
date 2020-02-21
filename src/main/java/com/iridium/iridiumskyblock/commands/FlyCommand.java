30cff6802a0a3ffafdd9cdc8a338dcc492eb6334
BGM <bgmp00@protonmail.com>
2020-02-21 19:07:48 -0300

    Discontinue the fly booster

--- src/main/java/com/iridium/iridiumskyblock/commands/FlyCommand.java
+++ src/main/java/com/iridium/iridiumskyblock/commands/FlyCommand.java
@@ -21,7 +21,7 @@ public class FlyCommand extends Command {
         User user = User.getUser(p);
         if (user.getIsland() != null) {
             if (user.getIsland().isInIsland(p.getLocation())) {
-                if (user.getIsland().getFlightBooster() != 0 || p.hasPermission("iridiumskyblock.Fly")) {
+                if (p.hasPermission("iridiumskyblock.Fly")) {
                     if (p.getAllowFlight()) {
                         p.setAllowFlight(false);
                         p.setFlying(false);
